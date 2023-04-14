22472ee64a164bcb43d2d79a567f86c9962a9de3
HasmH <h.hasam@outlook.com>
2023-04-15 02:28:31 +1000

    Initial Commit

---
+++ PositiveWordDaily/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PositiveWordDaily
+//
+//  Created by Hasam Hasam on 15/4/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PositiveWordDaily/PositiveWordDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PositiveWordDailyApp.swift
+//  PositiveWordDaily
+//
+//  Created by Hasam Hasam on 15/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct PositiveWordDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
